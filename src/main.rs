7aa16dd42af62ed9830bbafd05967f778af33c59
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2023-04-07 19:48:52 +0200

    Skeleton for first case test

--- src/main.rs
+++ src/main.rs
@@ -9,3 +9,11 @@ fn main() {
 fn unit_test() {
     todo!("This is yet to be thought up")
 }
+
+const _ENDPOINT: &str = "localhost:443";
+
+#[ignore = "Not yet written"]
+#[tokio::test]
+async fn use_case_1() {
+    let _client = reqwest::Client::builder().build().unwrap();
+}
