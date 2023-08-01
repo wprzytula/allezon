78cfe8447c7b43ca3c8a1d8a727501db29f70883
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2023-08-01 14:55:43 +0200

    tests: add test_complex

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,5 +1,9 @@
-use crate::types;
+use crate::{
+    endpoints::{Aggregate, Aggregates},
+    types::{Action, TimeRange, UtcMinute},
+};
 
+// mod client;
 mod dataset;
 mod test_data;
 mod utils;
@@ -12,10 +16,10 @@ async fn test_simple() {
     let cookie = utils::random_string(10);
 
     test_data
-        .create_user_tags_for_user(cookie.clone(), 1, Some(types::Action::Buy))
+        .create_user_tags_for_user(cookie.clone(), 1, Some(Action::Buy))
         .await;
     test_data
-        .create_user_tags_for_user(cookie.clone(), 1, Some(types::Action::View))
+        .create_user_tags_for_user(cookie.clone(), 1, Some(Action::View))
         .await;
     test_data.check_user_profile(cookie.clone(), 200).await;
 }
@@ -28,7 +32,39 @@ async fn test_200_last() {
     let cookie = utils::random_string(10);
 
     test_data
-        .create_user_tags_for_user(cookie.clone(), 201, Some(types::Action::Buy))
+        .create_user_tags_for_user(cookie.clone(), 201, Some(Action::Buy))
         .await;
     test_data.check_user_profile(cookie.clone(), 200).await;
+}
+
+#[tokio::test]
+async fn test_complex() {
+    let test_data = test_data::TestData::from_env().await;
+    test_data.clear().await;
+
+    let timestamp = chrono::Utc::now();
+    let timestamp_trunc = UtcMinute::from(timestamp);
+    let timestamp_trunc_next = timestamp_trunc.next();
+    let timerange = TimeRange {
+        from: timestamp_trunc.inner(),
+        to: timestamp_trunc_next.inner(),
+    };
+    let action = Action::Buy;
+
+    test_data
+        .create_user_tags_for_timestamp(timestamp, 201, Some(action))
+        .await;
+    test_data
+        .compare_aggregates(
+            timerange,
+            action,
+            Aggregates {
+                fst: Some(Aggregate::Count),
+                snd: Some(Aggregate::SumPrice),
+            },
+            None,
+            None,
+            None,
+        )
+        .await;
 }
